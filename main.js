70398db0d51a1a7e1ae2d8b49f1ec5df252903dc
robinNcode <msmrobin518@gmail.com>
2023-07-01 18:37:42 +0600

    `update`: Ice cream building done.

--- main.js
+++ main.js
@@ -2,18 +2,23 @@ const app = Vue.createApp({
     data() {
         return {
             variants: [
-                { id: 1, name: 'Vanila', price: 150},
+                { id: 1, name: 'Vanilla', price: 150},
                 { id: 2, name: 'Chocolate', price: 200},
                 { id: 3, name: 'Strawberry', price: 110},
                 { id: 4, name: 'Orange', price: 90},
-                { id: 5, name: 'Mango', price: 90}
+                { id: 5, name: 'Lemon', price: 30}
             ],
             cart: [],
         }
     },
     methods: {
         addToCart(variantId){
-            
+            this.cart.push(this.variants.find(variant => variant.id === variantId));
+        },
+        removeFromCart(variantId){
+            position = this.cart.findIndex(variant => variant.id === variantId);
+            // To remove the item from the cart ...
+            this.cart.splice(position, 1);
         }
     }
 });
