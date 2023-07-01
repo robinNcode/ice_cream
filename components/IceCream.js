380173f9dcc719e7a93a27e9bb3ea18953c4736a
robinNcode <msmrobin518@gmail.com>
2023-07-01 19:17:09 +0600

    Ice cream component splited

---
+++ components/IceCream.js
@@ -0,0 +1,15 @@
+app.component('ice-cream', {
+    props: {
+        cart: {
+            type: Array,
+            required: true
+        }
+    },
+    template: /*html*/
+        `<div class="icecream" id="icecream">
+            <p class="cone"></p>
+            <div class="scoop" v-for="cartItem in cart" :class="cartItem.name.toLowerCase()"></div>
+            <div class="cherry" v-if="cart.length > 0"></div>
+            <div class="question" v-else></div>
+        </div>`
+});
