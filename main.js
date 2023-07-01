b118eb527eeb058207afec260d9d455a2950ffd8
robinNcode <msmrobin518@gmail.com>
2023-07-01 18:20:35 +0600

    error solved

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
-const app = new Vue.createApp({
+const app = Vue.createApp({
     data() {
         return {
+            variants: [
+                { id: 1, name: 'Vanila', price: 150},
+                { id: 2, name: 'Chocolate', price: 200},
+                { id: 3, name: 'Strawberry', price: 110},
+                { id: 4, name: 'Orange', price: 90},
+                { id: 5, name: 'Mango', price: 90}
+            ],
             cart: [],
-            premium: true
         }
     },
     methods: {
         addToCart(variantId){
-
+            
         }
     }
 });
