ffbfc5cf3851e6a887ad847a9566223f7668eacf
robinNcode <msmrobin518@gmail.com>
2023-07-01 13:08:54 +0600

    Initial setup

---
+++ main.js
@@ -0,0 +1,13 @@
+const app = new Vue.createApp({
+    data() {
+        return {
+            cart: [],
+            premium: true
+        }
+    },
+    methods: {
+        addToCart(variantId){
+
+        }
+    }
+});
